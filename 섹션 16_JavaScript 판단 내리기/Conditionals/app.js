158ec255f6d9ab92a0a1ea6be21d94f3a4210859
me <me@me>
2025-12-08 11:47:19 +0000

    2025-12-08 study 2

--- 섹션 16_JavaScript 판단 내리기/Conditionals/app.js
+++ 섹션 16_JavaScript 판단 내리기/Conditionals/app.js
@@ -1,21 +1,46 @@
 //console.log("ITS WORKING")
-console.log("Before Conditional")
-let random = Math.random();
-if (random < 0.5) {
-    console.log("YOUR NUMBER IS LESS THAN 0.5!!!");
-    console.log(random);
-}
-// if (1 + 1 === 2) {
-//     console.log("MATH STILL WORKS!")
+// console.log("Before Conditional")
+// let random = Math.random();
+// if (random < 0.5) {
+//     console.log("YOUR NUMBER IS LESS THAN 0.5!!!");
+//     console.log(random);
 // }
-if (random >= 0.5) {
-    console.log("YOUR NUMBER IS GRATHER THAN 0.5!!");
-    console.log(random);
+// // if (1 + 1 === 2) {
+// //     console.log("MATH STILL WORKS!")
+// // }
+// if (random >= 0.5) {
+//     console.log("YOUR NUMBER IS GRATHER THAN 0.5!!");
+//     console.log(random);
+// }
+
+// console.log("After Conditional")
+
+const dayOfWeek = 'Friday';
+
+if (dayOfWeek === 'Monday') {
+    console.log("I HATE MONDAYS!");
+}
+else if (dayOfWeek === 'Saturday') {
+    console.log("YAY I LOVE SATURDYAS!");
+}
+else if (dayOfWeek === 'Friday') {
+    console.log("FRIDAYS ARE DECENT, ESPECIALLY AFTER WORK!");
 }
 
-console.log("After Conditional")
+// 0-5 - Free
+// 5 - 10 Child $10
+// 10 - 65 ADULT $20
+// 65+ SENIOR $10
 
+const age = 8;
 
-
-
+if (age < 5) {
+    console.log("You are a baby.");
+}
+else if (age < 10) {
+    console.log("You are a child")
+}
+else if (age < 65) {
+    console.log("You are an adult");
+}
 
--- 섹션 16_JavaScript 판단 내리기/section16.md
+++ 섹션 16_JavaScript 판단 내리기/section16.md
@@ -64,3 +64,10 @@ let rating = 3;
 if (rating === 3) {
     console.log("YOU ARE A SUPERSTAR!!");
 }
+else if (rating === 2) {
+    console.log("MEETS EXPECTATIONS");
+}
+else if (rating === 1) {
+    console.log("NEEDS IMPROVEMENT");
+}
+```
