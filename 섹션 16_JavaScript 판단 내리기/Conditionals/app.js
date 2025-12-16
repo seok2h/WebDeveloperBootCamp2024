5ab1c3e32084e90f2de8e2f13d9fbcbb77a97621
Han Seungseok <cream5432111@gmail.com>
2025-12-16 18:30:14 +0900

    2025-12-16 study 1

--- 섹션 16_JavaScript 판단 내리기/Conditionals/app.js
+++ 섹션 16_JavaScript 판단 내리기/Conditionals/app.js
@@ -1,51 +1,64 @@
-//console.log("ITS WORKING")
-// console.log("Before Conditional")
-// let random = Math.random();
-// if (random < 0.5) {
-//     console.log("YOUR NUMBER IS LESS THAN 0.5!!!");
-//     console.log(random);
-// }
-// // if (1 + 1 === 2) {
-// //     console.log("MATH STILL WORKS!")
+// //console.log("ITS WORKING")
+// // console.log("Before Conditional")
+// // let random = Math.random();
+// // if (random < 0.5) {
+// //     console.log("YOUR NUMBER IS LESS THAN 0.5!!!");
+// //     console.log(random);
 // // }
-// if (random >= 0.5) {
-//     console.log("YOUR NUMBER IS GRATHER THAN 0.5!!");
-//     console.log(random);
+// // // if (1 + 1 === 2) {
+// // //     console.log("MATH STILL WORKS!")
+// // // }
+// // if (random >= 0.5) {
+// //     console.log("YOUR NUMBER IS GRATHER THAN 0.5!!");
+// //     console.log(random);
+// // }
+
+// // console.log("After Conditional")
+
+// const dayOfWeek = 'Friday';
+
+// if (dayOfWeek === 'Monday') {
+//     console.log("I HATE MONDAYS!");
+// }
+// else if (dayOfWeek === 'Saturday') {
+//     console.log("YAY I LOVE SATURDYAS!");
+// }
+// else if (dayOfWeek === 'Friday') {
+//     console.log("FRIDAYS ARE DECENT, ESPECIALLY AFTER WORK!");
+// }
+// else {
+//     console.log("MEH");
 // }
 
-// console.log("After Conditional")
+// // 0-5 - Free
+// // 5 - 10 Child $10
+// // 10 - 65 ADULT $20
+// // 65+ SENIOR $10
 
-const dayOfWeek = 'Friday';
+// const age = 8;
 
-if (dayOfWeek === 'Monday') {
-    console.log("I HATE MONDAYS!");
-}
-else if (dayOfWeek === 'Saturday') {
-    console.log("YAY I LOVE SATURDYAS!");
-}
-else if (dayOfWeek === 'Friday') {
-    console.log("FRIDAYS ARE DECENT, ESPECIALLY AFTER WORK!");
-}
-else {
-    console.log("MEH");
-}
+// if (age < 5) {
+//     console.log("You are a baby.");
+// }
+// else if (age < 10) {
+//     console.log("You are a child")
+// }
+// else if (age < 65) {
+//     console.log("You are an adult");
+// }
+// else {
+//     console.log("You are a senior");
+// }
 
-// 0-5 - Free
-// 5 - 10 Child $10
-// 10 - 65 ADULT $20
-// 65+ SENIOR $10
+const password = prompt("please enter a new password");
 
-const age = 8;
-
-if (age < 5) {
-    console.log("You are a baby.");
+if (password.length >= 6) {
+    if (password.indexOf(' ') === -1) {
+        console.log("Valid Password!");
+    } else {
+        console.log("Password cannot contain spaces!");
+    }
+} else {
+    console.log("PASSWORD TOO SHORT! Must be 6+ character")
 }
-else if (age < 10) {
-    console.log("You are a child")
-}
-else if (age < 65) {
-    console.log("You are an adult");
-}
-else {
-    console.log("You are a senior");
-}
+// password cannot include space
