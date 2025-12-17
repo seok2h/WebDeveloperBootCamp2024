4a3ef5eb23043cc92704fe9ae467477e1807cc74
Han Seungseok <cream5432111@gmail.com>
2025-12-17 16:28:19 +0900

    2025-12-17 study 1

--- 섹션 16_JavaScript 판단 내리기/Logical_Operators/app.js
+++ 섹션 16_JavaScript 판단 내리기/Logical_Operators/app.js
@@ -6,14 +6,25 @@
 //     console.log("INCORRECT FORMAT FOR PASSWORD!");
 // }
 
-const age = 90;
+// const age = 90;
 
-if (age >= 0 && age < 5 || age >= 65) {
-    console.log("FREE");
-} else if (age >= 5 && age < 10) {
-    console.log("$10");
-} else if (age >= 10 && age < 65) {
-    console.log("$20");
-} else {
-    console.log("INVALID AGE!!");
+// if (age >= 0 && age < 5 || age >= 65) {
+//     console.log("FREE");
+// } else if (age >= 5 && age < 10) {
+//     console.log("$10");
+// } else if (age >= 10 && age < 65) {
+//     console.log("$20");
+// } else {
+//     console.log("INVALID AGE!!");
+// }
+
+// const firstName = prompt("enter your first name");
+// if (!firstName) {
+//     firstName = prompt("TRY AGAIN!!");
+// }
+
+const age = 45;
+if (!(age >= 0 && age < 5 || age >= 65)) {
+    console.log("YOU ARE NOT A BABY OR A SENIOR!");
 }
+
