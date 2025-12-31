f38deb4faf3f8912d45bb795b51bfdbba7dab174
seok2h <cream5432111@gmail.com>
2025-12-31 18:30:17 +0900

    2025-12-31 study 7

--- 섹션 19_루프의 구조/app.js
+++ 섹션 19_루프의 구조/app.js
@@ -27,17 +27,21 @@ while (!maximum) {
 const targetNum = Math.floor(Math.random() * maximum) + 1;
 console.log(targetNum)
 
-let guess = prompt("Enter your first guess!");
+let guess = prompt("Enter your first guess! (Type 'q' to quit)");
 let attempts = 1;
 
 while (parseInt(guess) !== targetNum) {
     if (guess === 'q') break;
+    guess = parseInt(guess);
 
-    attempts++;
     if (guess > targetNum) {
         guess = prompt("Too high! Enter a new guess:");
-    } else {
+        attempts++;
+    } else if (guess < targetNum) {
         guess = prompt("Too low! Enter a new guess!");
+        attempts++;
+    } else {
+        guess = prompt("Invalid guess. Please enter a number or 'q' to quit");
     }
 }
 
