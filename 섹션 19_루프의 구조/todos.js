831be411134faa502d614d25056eb3e068483782
seok2h <cream5432111@gmail.com>
2026-01-01 17:29:54 +0900

    2026-01-01 study 1

--- 섹션 19_루프의 구조/todos.js
+++ 섹션 19_루프의 구조/todos.js
@@ -8,6 +8,18 @@ while (input !== 'quit' && input !== 'q') {
             console.log(`${i}: ${todos[i]}`)
         }
         console.log('****************');
+    } else if (input === 'new') {
+        const newTodo = prompt('Ok, what is the new todo?');
+        todos.push(newTodo);
+        console.log(`${newTodo} added to the list!`);
+    } else if (input === 'delete') {
+        const index = parseInt(prompt('Ok, enter an index to delete:'));
+        if (Number.isNaN(index)) {
+            const deleted = todos.splice(index, 1);
+            console.log(`Ok, deleted ${deleted[0]}`);
+        } else {
+            console.log('Unknown index');
+        }
     }
     input = prompt("what would you like to do?");
 }
